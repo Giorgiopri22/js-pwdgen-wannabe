73458b53c512eb8b03b319c03d7e982b4fcf0b29
Giorgiopri22 <giorgioprizzi@gmail.com>
2023-03-21 13:43:35 +0100

    Fine es

---
+++ main.js
@@ -0,0 +1,7 @@
+let nome = prompt( "quale è il tuo nome?")
+let cognome = prompt("quale è il tuo cognome?")
+let colorePreferito = prompt("quale è il tuo colore preferito?")
+
+
+document.getElementById('password').value = ` ${nome}${cognome}${colorePreferito}21 `;
+console.log("bella mattia");
